395a29d85a86f695c7a46386da9de20567701794
Krishna Kumar <kkumar@exathink.com>
2018-04-20 10:46:26 -0500

    Update account activity summary by organization to use contributor_key sensitive logic for calculating org contributor counts [ #156937793 (https://www.pivotaltracker.com/story/show/156937793)]

--- polaris/analytics/datasources/accounts/activity_summary.py
+++ polaris/analytics/datasources/accounts/activity_summary.py
@@ -27,9 +27,9 @@ class ActivitySummaryByOrganization(Schema):
         query = text(
             """
               SELECT
-                organizations.id as organization_id,
+                organizations.id AS organization_id,
                 organizations.organization_key,
-                organizations.name as organization,
+                organizations.name AS organization,
                 earliest_commit,
                 latest_commit,
                 commit_count,
@@ -47,15 +47,37 @@ class ActivitySummaryByOrganization(Schema):
                   GROUP BY organization_id
             ) AS org_repo_summary
                 ON organizations.id = org_repo_summary.organization_id
-            LEFT OUTER JOIN (
+            INNER JOIN (
                 SELECT
-                  organization_id,
-                  count(DISTINCT contributor_alias_id) AS contributor_count
+                 organization_id,
+                 sum(contributor_count) AS contributor_count
                 FROM
-                  repos.repositories
-                  INNER JOIN repos.repositories_contributor_aliases
-                  ON repositories.id = repositories_contributor_aliases.repository_id
-                GROUP BY repositories.organization_id
+                  (
+                    SELECT
+                      organization_id,
+                      count(DISTINCT contributor_alias_id) AS contributor_count
+                    FROM
+                      repos.repositories
+                      INNER JOIN repos.repositories_contributor_aliases
+                        ON repositories.id = repositories_contributor_aliases.repository_id
+                      INNER JOIN repos.contributor_aliases
+                        ON repositories_contributor_aliases.contributor_alias_id = contributor_aliases.id
+                    WHERE contributor_aliases.contributor_key IS NULL
+                    GROUP BY repositories.organization_id
+                    UNION
+                    SELECT
+                      organization_id,
+                      count(DISTINCT contributor_key) AS contributor_count
+                    FROM
+                      repos.repositories
+                      INNER JOIN repos.repositories_contributor_aliases
+                        ON repositories.id = repositories_contributor_aliases.repository_id
+                      INNER JOIN repos.contributor_aliases
+                        ON repositories_contributor_aliases.contributor_alias_id = contributor_aliases.id
+                    WHERE contributor_aliases.contributor_key IS NOT NULL
+                    GROUP BY repositories.organization_id
+                  ) _
+                GROUP BY organization_id
             ) org_contributor_summary
             ON org_repo_summary.organization_id = org_contributor_summary.organization_id
           """
@@ -68,47 +90,73 @@ class ActivitySummaryByOrganization(Schema):
         query = text(
             """
                 SELECT
-                  org_repo_summary.organization_id as organization_id,
-                  org_repo_summary.organization_key as organization_key,
-                  org_repo_summary.organization as organization,
-                  earliest_commit,
-                  latest_commit,
-                  commit_count,
-                  contributor_count
-                FROM (
-                  SELECT
-                    organizations.id as organization_id,
-                    min(organizations.organization_key :: TEXT) AS organization_key,
-                    min(organizations.name)                     AS organization,
-                    min(earliest_commit)                        AS earliest_commit,
-                    max(latest_commit)                          AS latest_commit,
-                    sum(commit_count)                           AS commit_count
-                  FROM repos.repositories
-                    INNER JOIN repos.organizations ON repositories.organization_id = organizations.id
-                    INNER JOIN repos.accounts_organizations ON organizations.id = accounts_organizations.organization_id
-                    INNER JOIN repos.accounts ON accounts_organizations.account_id = accounts.id
-                  WHERE account_key = :account_key
-                  GROUP BY organizations.id
-                ) as org_repo_summary
-                INNER JOIN (
-                  SELECT
-                    organizations.id as organization_id,
-                    count(distinct contributor_alias_id) as contributor_count
-                  FROM
-                    repos.repositories_contributor_aliases
-                    INNER JOIN repos.repositories on repositories_contributor_aliases.repository_id = repositories.id
-                    INNER JOIN repos.organizations ON repositories.organization_id = organizations.id
-                    INNER JOIN repos.accounts_organizations ON organizations.id = accounts_organizations.organization_id
-                    INNER JOIN repos.accounts ON accounts_organizations.account_id = accounts.id
-                  WHERE account_key = :account_key
-                  GROUP BY organizations.id
-                ) as org_contributor_summary
-                on org_repo_summary.organization_id = org_contributor_summary.organization_id
+                      org_repo_summary.organization_id  AS organization_id,
+                      org_repo_summary.organization_key AS organization_key,
+                      org_repo_summary.organization     AS organization,
+                      earliest_commit,
+                      latest_commit,
+                      commit_count,
+                      contributor_count
+                FROM
+                (
+                   SELECT
+                     organizations.id                            AS organization_id,
+                     min(organizations.organization_key :: TEXT) AS organization_key,
+                     min(organizations.name)                     AS organization,
+                     min(earliest_commit)                        AS earliest_commit,
+                     max(latest_commit)                          AS latest_commit,
+                     sum(commit_count)                           AS commit_count
+                   FROM repos.repositories
+                     INNER JOIN repos.organizations ON repositories.organization_id = organizations.id
+                     INNER JOIN repos.accounts_organizations ON organizations.id = accounts_organizations.organization_id
+                     INNER JOIN repos.accounts ON accounts_organizations.account_id = accounts.id
+                   WHERE account_key = :account_key
+                   GROUP BY organizations.id
+                ) AS org_repo_summary
+                INNER JOIN
+                (
+                   SELECT
+                     organization_id,
+                     sum(contributor_count) AS contributor_count
+                   FROM
+                     (
+                       SELECT
+                         repositories.organization_id,
+                         count(DISTINCT contributor_alias_id) AS contributor_count
+                       FROM
+                         repos.repositories
+                         INNER JOIN repos.repositories_contributor_aliases
+                           ON repositories.id = repositories_contributor_aliases.repository_id
+                         INNER JOIN repos.contributor_aliases
+                           ON repositories_contributor_aliases.contributor_alias_id = contributor_aliases.id
+                         INNER JOIN repos.organizations ON repositories.organization_id = organizations.id
+                         INNER JOIN repos.accounts_organizations
+                           ON accounts_organizations.organization_id = organizations.id
+                         INNER JOIN repos.accounts ON accounts_organizations.account_id = accounts.id
+                       WHERE account_key = :account_key AND contributor_aliases.contributor_key IS NULL
+                       GROUP BY repositories.organization_id
+                       UNION
+                       SELECT
+                         repositories.organization_id,
+                         count(DISTINCT contributor_key) AS contributor_count
+                       FROM
+                         repos.repositories
+                         INNER JOIN repos.repositories_contributor_aliases
+                           ON repositories.id = repositories_contributor_aliases.repository_id
+                         INNER JOIN repos.contributor_aliases
+                           ON repositories_contributor_aliases.contributor_alias_id = contributor_aliases.id
+                         INNER JOIN repos.organizations ON repositories.organization_id = organizations.id
+                         INNER JOIN repos.accounts_organizations
+                           ON accounts_organizations.organization_id = organizations.id
+                         INNER JOIN repos.accounts ON accounts_organizations.account_id = accounts.id
+                       WHERE account_key = :account_key AND contributor_aliases.contributor_key IS NOT NULL
+                       GROUP BY repositories.organization_id
+                     ) _
+                   GROUP BY organization_id
+                ) AS org_contributor_summary
+                ON org_repo_summary.organization_id = org_contributor_summary.organization_id
             """
         )
         with db.create_session() as session:
             results = session.connection.execute(query, dict(account_key=account_key)).fetchall()
             return self.dumps(results, many=True)
-
-
-

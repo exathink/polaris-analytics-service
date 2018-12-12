79190cef1df5f05e8df36bea4edbec5f3fd90427
Krishna Kumar <kkumar@exathink.com>
2018-12-12 17:54:59 -0600

    Implement import new commits
    [#162432738]

---
+++ polaris/analytics/db/api.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2011-2018) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+from polaris.common import db
+from polaris.analytics.db import impl
+from sqlalchemy.exc import SQLAlchemyError
+
+def import_new_commits(organization_key, repository_key, new_commits, new_contributors):
+    try:
+        with db.create_session() as session:
+            result = impl.import_new_commits(session, organization_key, repository_key, new_commits, new_contributors)
+            return dict(
+                success=True
+            )
+
+    except SQLAlchemyError as exc:
+        return db.process_exception("Import new_commits failed", exc)
+    except Exception as e:
+        return db.failure_message('Import new_commits failed', e)
+
+
---
+++ polaris/analytics/db/impl/commit_import.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2011-2018) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+import uuid
+from polaris.common import db
+from polaris.utils.collections import dict_select
+from polaris.analytics.db.model import commits, contributors
+from sqlalchemy import Column, select, BigInteger, Integer
+
+from sqlalchemy.dialects.postgresql import insert
+
+def import_new_contributors(session, new_contributors):
+    if len(new_contributors) > 0:
+        session.connection.execute(
+            insert(contributors).values([
+                dict(
+                    key=contributor['contributor_key'],
+                    name=contributor['name'],
+                    source='vcs',
+                    source_alias=contributor['alias']
+                )
+                for contributor in new_contributors
+            ]).on_conflict_do_nothing(
+                index_elements=['key']
+            )
+        )
+
+def import_new_commits(session, organization_key, repository_key, new_commits, new_contributors):
+
+    import_new_contributors(session, new_contributors)
+
+    commits_temp = db.temp_table_from(
+        commits,
+        table_name='commits_temp',
+        exclude_columns=[commits.c.id, commits.c.committer_contributor_id, commits.c.author_contributor_id],
+        extra_columns=[
+            Column('committer_contributor_id', Integer, nullable=True),
+            Column('author_contributor_id', Integer, nullable=True)
+        ]
+    )
+    commits_temp.create(session.connection, checkfirst=True)
+
+    session.connection.execute(
+        commits_temp.insert([
+            dict(
+                repository_key=repository_key,
+                key = uuid.uuid4().hex,
+                source_id=commit['commit_key'],
+                **dict_select(
+                    commit, [
+                        'commit_date',
+                        'commit_date_tz_offset',
+                        'committer_contributor_key',
+                        'committer_contributor_name',
+                        'author_date',
+                        'author_date_tz_offset',
+                        'author_contributor_key',
+                        'author_contributor_name',
+                        'parents',
+                        'stats',
+                        'commit_message',
+                        'created_at',
+                        'created_on_branch'
+                    ]
+                )
+            )
+            for commit in new_commits
+        ])
+    )
+
+    # resolve committer_keys
+    session.connection.execute(
+        commits_temp.update().values(
+            committer_contributor_id=select([
+                contributors.c.id.label('committer_contributor_id')
+            ]).where(
+                contributors.c.key == commits_temp.c.committer_contributor_key
+            ).limit(1)
+        )
+    )
+
+    # resolve author_keys
+    session.connection.execute(
+        commits_temp.update().values(
+            author_contributor_id=select([
+                contributors.c.id.label('author_contributor_id')
+            ]).where(
+                contributors.c.key == commits_temp.c.author_contributor_key
+            ).limit(1)
+        )
+    )
+
+    session.connection.execute(
+        insert(commits).from_select(
+            [column.name for column in commits_temp.columns],
+            select([commits_temp])
+        ).on_conflict_do_nothing(
+            index_elements=['repository_key', 'source_id']
+        )
+    )
+
+
+

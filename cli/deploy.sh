0b66769e87259f2151e49e8465d36a22133c49c6
Krishna Kumar <kkumar@exathink.com>
2019-01-03 20:47:22 -0600

    Implemented deploy aws cli command [ #162955648 (https://www.pivotaltracker.com/story/show/162955648)]

---
+++ cli/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+package aws deploy-image
+package aws deploy-task-definition polaris-analytics-service
+package aws deploy-task-definition polaris-analytics-listener
+package aws deploy-task-definition polaris-analytics-db-migrator
+

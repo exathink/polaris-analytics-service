e1bf7c9da8f13b6e6209afa6b9b6ae075dfe201d
Krishna Kumar <kkumar@exathink.com>
2019-02-06 12:14:00 -0600

    Update deploy.sh to run migrations and deploy services.

--- cli/deploy.sh
+++ cli/deploy.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
+set -e
 
+echo "deploying image"
 package aws deploy-image
+
+echo "deploying task definitions"
 package aws deploy-task-definition polaris-analytics-service
 package aws deploy-task-definition polaris-analytics-listener
 package aws deploy-task-definition polaris-analytics-db-migrator
 
+echo "Running migrations"
+package aws run-task polaris-analytics-db-migrator upgrade head
+
+echo "Deploying Services.."
+package aws deploy-services polaris-analytics-service polaris.service.auto-scaling-group polaris-analytics-service polaris-analytics-listener

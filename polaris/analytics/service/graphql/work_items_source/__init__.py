6345d89638a40e3bc49edf89b202a510309f0240
Krishna Kumar <kkumar@exathink.com>
2019-05-15 09:14:13 -0500

    Add missing package initializer file.
    [
    story=#165870191 subject=atlassian-connect-from-ui story_url=https://www.pivotaltracker.com/story/show/165870191
    ]

---
+++ polaris/analytics/service/graphql/work_items_source/__init__.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Copyright: © Exathink, LLC (2019) All Rights Reserved
+
+# Unauthorized use or copying of this file and its contents, via any medium
+# is strictly prohibited. The work product in this file is proprietary and
+# confidential.
+
+# Author: Krishna Kumar
+
+import graphene
+from polaris.analytics.service.graphql.interface_mixins import NamedNodeResolverMixin
+from polaris.graphql.selectable import Selectable, CountableConnection, ConnectionResolverMixin
+from polaris.graphql.interfaces import NamedNode
+from polaris.analytics.service.graphql.interfaces import WorkTrackingIntegrationType
+
+from .selectable import WorkItemsSourceNode
+
+
+
+class WorkItemsSource(
+    # interface mixins
+    NamedNodeResolverMixin,
+
+    Selectable
+):
+    class Meta:
+        interfaces = (NamedNode, )
+        interface_resolvers = {}
+        named_node_resolver = WorkItemsSourceNode
+        connection_class = lambda : WorkItemsSources
+
+
+class WorkItemsSources(
+    CountableConnection
+):
+    class Meta:
+        node = WorkItemsSource
+
+
+class WorkItemsSourcesConnectionMixin(ConnectionResolverMixin):
+    work_items_sources = WorkItemsSource.ConnectionField(
+        integration_type=graphene.Argument(
+            WorkTrackingIntegrationType, required=False,
+            description='filter the work items sources by integration type'
+        ),
+    )
+
+    def resolve_work_items_sources(self, info, **kwargs):
+        return WorkItemsSource.resolve_connection(
+            self.get_connection_resolver_context('work_items_sources'),
+            self.get_connection_node_resolver('work_items_sources'),
+            self.get_instance_query_params(),
+            **kwargs
+        )

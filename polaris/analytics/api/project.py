d584c015bc22e679e39bdf038807fd842f2bce02
Maneet Sehgal <maneet@64sqs.com>
2020-01-29 10:21:18 +0530

    PO-18: Updated mutation logic to use lambda

--- polaris/analytics/api/project.py
+++ polaris/analytics/api/project.py
@@ -12,7 +12,7 @@ import logging
 from polaris.common import db
 from polaris.analytics.db.model import Project, WorkItemsSource
 from polaris.utils.exceptions import ProcessingException
-from polaris.analytics import api
+from polaris.utils.collections import find
 
 logger = logging.getLogger('polaris.analytics.api.project')
 
@@ -33,22 +33,17 @@ def update_project_state_maps(project_state_maps, join_this=None):
 
         # Check if project exists. Not sure if this is required
         project_key = project_state_maps.project_key
-        logger.info(project_key)
         project = Project.find_by_project_key(session, project_key)
-        logger.info(project)
         if project is None:
             raise ProcessingException(f'Could not find project with key: {project_key}')
 
         # Find and update corresponding work items source state maps
         for work_items_source_state_map in project_state_maps.work_items_source_state_maps:
             source_key = work_items_source_state_map.work_items_source_key
-            work_items_source = WorkItemsSource.find_by_work_items_source_key(session, source_key)
+            work_items_source = find(project.work_items_sources, lambda work_item_source: str(work_item_source.key) == str(source_key))
+            logger.info(work_items_source)
             if work_items_source is not None:
-                project_work_items_source = WorkItemsSource.find_by_project_id(session, project.id, source_key)
-                if project_work_items_source is not None:
-                    project_work_items_source.init_state_map(work_items_source_state_map.state_maps)
-                else:
-                    raise ProcessingException(f'Work item source with key: {source_key} is not associated to project with key: {project_key}')
+                work_items_source.init_state_map(work_items_source_state_map.state_maps)
             else:
-                raise ProcessingException(f'Could not find work item source with key: {source_key}')
+                raise ProcessingException(f'Work item source with key: {source_key} is not associated to project with key: {project_key}')
     return True

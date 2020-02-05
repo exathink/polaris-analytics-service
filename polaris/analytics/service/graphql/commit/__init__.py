192c08654a30b700f73635989a3d71f789ae52f4
Krishna Kumar <kkumar@exathink.com>
2020-02-05 14:12:15 -0600

    simplify CommitInfoResolverMixins to only resolve the fields that need special processing [story=PO-36 subject=refactor_interface_mixins_out story_url=https://urjuna.atlassian.net/browse/PO-36]

--- polaris/analytics/service/graphql/commit/__init__.py
+++ polaris/analytics/service/graphql/commit/__init__.py
@@ -15,7 +15,7 @@ from polaris.graphql.selectable import Selectable, ConnectionResolverMixin
 from polaris.graphql.connection_utils import CountableConnection
 from polaris.graphql.utils import create_tuple, init_tuple
 
-from ..interfaces import CommitInfo,CommitChangeStats, FileTypesSummary, \
+from ..interfaces import CommitInfo, CommitChangeStats, FileTypesSummary, \
     CommitWorkItemsSummary
 from ..interface_mixins import KeyIdResolverMixin
 
@@ -31,63 +31,15 @@ class CommitInfoResolverMixin(KeyIdResolverMixin):
         super().__init__(*args, **kwargs)
         self.commit_info = init_tuple(self.commit_tuple, **kwargs)
 
-    def resolve_commit(self, info, **kwargs):
-        return self.resolve_interface_for_instance(
-            interface=['Commit'],
-            params=self.get_instance_query_params(),
-            **kwargs
-        )
-
-    def get_commit(self, info, **kwargs):
-        if self.commit_info is None:
-            self.commit_info = self.resolve_commit(info, **kwargs)
-        return self.commit_info
-
-    def resolve_commit_hash(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).commit_hash
-
-    def resolve_repository(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).repository
-
-    def resolve_repository_key(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).repository_key
-
-    def resolve_repository_url(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).repository_url
-
-    def resolve_commit_date(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).commit_date
-
-    def resolve_committer(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).committer
-
-    def resolve_committer_key(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).committer_key
-
-    def resolve_author_date(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).author_date
-
-    def resolve_author(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).author
-
-    def resolve_author_key(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).author_key
-
-    def resolve_commit_message(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).commit_message
-
-    def resolve_num_parents(self, info, **kwargs):
-        return self.get_commit(info, **kwargs).num_parents
-
     def resolve_stats(self, info, **kwargs):
-        return init_tuple(self.stats_tuple, **(self.get_commit(info, **kwargs).stats or self.no_stats))
+        return init_tuple(self.stats_tuple, **(self.commit_info.stats or self.no_stats))
 
     def resolve_file_types_summary(self, info, **kwargs):
-        file_types_summary = self.get_commit(info, **kwargs).file_types_summary or []
+        file_types_summary = self.commit_info.file_types_summary or []
         return [FileTypesSummary(**summary) for summary in file_types_summary]
 
     def resolve_work_items_summaries(self, info, **kwargs):
-        work_items_summaries = self.get_commit(info, **kwargs).work_items_summaries or []
+        work_items_summaries = self.commit_info.work_items_summaries or []
         return [CommitWorkItemsSummary(**summary) for summary in work_items_summaries]
 
 
@@ -120,8 +72,8 @@ class Commits(
     class Meta:
         node = Commit
 
-class CommitsConnectionMixin(KeyIdResolverMixin, ConnectionResolverMixin):
 
+class CommitsConnectionMixin(KeyIdResolverMixin, ConnectionResolverMixin):
     commits = Commit.ConnectionField(
         before=graphene.Argument(
             graphene.DateTime, required=False,

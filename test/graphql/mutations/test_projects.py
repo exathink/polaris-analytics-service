219b87b250625d43204525c958478d23618c337b
Krishna Kumar <kkumar@exathink.com>
2020-02-19 10:46:11 -0600

    Added more tests for the update_state_type_on_work_items cases. Removed GraphQL based assertion testing and replaced with SQL [story=PO-53 subject=whitelist_updates_for_polaris.exathink.com story_url=https://urjuna.atlassian.net/browse/PO-53]

--- test/graphql/mutations/test_projects.py
+++ test/graphql/mutations/test_projects.py
@@ -56,61 +56,16 @@ def setup_work_items_sources(setup_projects):
         session.add(project)
         project.work_items_sources.append(
             WorkItemsSource(
-                    organization_key=str(organization.key),
-                    organization_id=organization.id,
-                    key=str(uuid.uuid4()),
-                    name='foo',
-                    integration_type='jira',
-                    work_items_source_type='repository_issues',
-                    commit_mapping_scope='repository',
-                    source_id=str(uuid.uuid4())
+                organization_key=str(organization.key),
+                organization_id=organization.id,
+                key=str(uuid.uuid4()),
+                name='foo',
+                integration_type='jira',
+                work_items_source_type='repository_issues',
+                commit_mapping_scope='repository',
+                source_id=str(uuid.uuid4())
             )
         )
-    yield project
-
-
-@pytest.yield_fixture()
-def setup_work_items(setup_projects):
-    organization = setup_projects
-    project = organization.projects[0]
-    # open state_type of this new work item should be updated to complete after the test
-    new_work_items = [
-        dict(
-            key=uuid.uuid4().hex,
-            name='Issue 10',
-            display_id='1000',
-            created_at=get_date("2018-12-02"),
-            updated_at=get_date("2018-12-03"),
-            is_bug=True,
-            work_item_type='issue',
-            url='http://foo.com',
-            tags=['ares2'],
-            state='done',
-            description='foo',
-            source_id=str(uuid.uuid4()),
-            state_type='open'
-          )
-    ]
-    with db.orm_session() as session:
-        session.expire_on_commit = False
-        session.add(organization)
-        session.add(project)
-        project.work_items_sources.append(
-            WorkItemsSource(
-                    organization_key=str(organization.key),
-                    organization_id=organization.id,
-                    key=str(uuid.uuid4()),
-                    name='foo',
-                    integration_type='jira',
-                    work_items_source_type='repository_issues',
-                    commit_mapping_scope='repository',
-                    source_id=str(uuid.uuid4())
-            )
-        )
-        project.work_items_sources[0].work_items.extend([
-            WorkItem(**item)
-            for item in new_work_items
-        ])
     yield project
 
 
@@ -126,9 +81,9 @@ class TestArchiveProject:
                 }
             }
         """, variable_values=dict(
-                archiveProjectInput=dict(
-                    projectKey=str(test_projects[0]['key'])
-                )
+            archiveProjectInput=dict(
+                projectKey=str(test_projects[0]['key'])
+            )
         ))
         assert response['data']['archiveProject']['projectName'] == 'mercury'
         assert db.connection().execute(
@@ -137,52 +92,6 @@ class TestArchiveProject:
 
 
 class TestUpdateProjectStateMaps:
-    def it_updates_project_work_item_state(self, setup_work_items):
-        client = Client(schema)
-        project = setup_work_items
-        project_key = str(project.key)
-        work_items_source_key = project.work_items_sources[0].key
-        work_item_key = project.work_items_sources[0].work_items[0].key
-        response = client.execute("""
-            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
-                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
-                        success
-                    }
-                }
-        """, variable_values=dict(
-                    updateProjectStateMapsInput=dict(
-                        projectKey=project_key,
-                        workItemsSourceStateMaps=[
-                         dict(
-                             workItemsSourceKey=work_items_source_key,
-                             stateMaps=[
-                                 dict(state="todo", stateType=WorkItemsStateType.open.value),
-                                 dict(state="doing", stateType=WorkItemsStateType.wip.value),
-                                 dict(state="done", stateType=WorkItemsStateType.complete.value)
-                             ]
-                         )
-                        ]
-                    )
-        )
-        )
-        assert 'data' in response
-        result = response['data']['updateProjectStateMaps']
-        assert result
-        assert result['success']
-
-        query = """
-            query getWorkItem($key:String!) {
-                workItem(key: $key, interfaces: [WorkItemInfo]){
-                    description
-                    displayId
-                    state
-                    stateType
-                }
-            } 
-        """
-        work_item_result = client.execute(query, variable_values=dict(key=work_item_key))
-        assert 'data' in work_item_result
-        assert work_item_result['data']['workItem']['stateType'] == WorkItemsStateType.complete.value
 
     def it_updates_project_work_items_source_state_map(self, setup_work_items_sources):
         client = Client(schema)
@@ -216,7 +125,6 @@ class TestUpdateProjectStateMaps:
         assert result
         assert result['success']
 
-
     def it_checks_if_project_exists(self, setup_work_items_sources):
         client = Client(schema)
         project = setup_work_items_sources
@@ -244,13 +152,12 @@ class TestUpdateProjectStateMaps:
                 ]
             )
         )
-    )
+                                  )
         assert 'data' in response
         result = response['data']['updateProjectStateMaps']
         assert result
         assert not result['success']
-        assert result['errorMessage'] == "Could not find project with key %s" %project_key
-
+        assert result['errorMessage'] == f"Could not find project with key {project_key}"
 
     def it_checks_if_work_items_source_belongs_to_project(self, setup_work_items_sources):
         client = Client(schema)
@@ -258,12 +165,12 @@ class TestUpdateProjectStateMaps:
         project_key = str(project.key)
         work_items_source_key = str(uuid.uuid4())
         response = client.execute("""
-                            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
-                                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
-                                        success
-                                        errorMessage
-                                    }
-                                }
+                    mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
+                                    updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
+                                success
+                                errorMessage
+                            }
+                        }
                         """, variable_values=dict(
             updateProjectStateMapsInput=dict(
                 projectKey=project_key,
@@ -283,7 +190,8 @@ class TestUpdateProjectStateMaps:
         assert 'data' in response
         result = response['data']['updateProjectStateMaps']
         assert not result['success']
-        assert result['errorMessage'] == f"Work Items Source with key {work_items_source_key} does not belong to project"
+        assert result[
+                   'errorMessage'] == f"Work Items Source with key {work_items_source_key} does not belong to project"
 
     def it_checks_if_work_items_source_states_have_duplicates(self, setup_work_items_sources):
         client = Client(schema)
@@ -291,12 +199,12 @@ class TestUpdateProjectStateMaps:
         project_key = str(project.key)
         work_items_source_key = project.work_items_sources[0].key
         response = client.execute("""
-                            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
-                                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
-                                        success
-                                        errorMessage
-                                    }
-                                }
+                    mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
+                                    updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
+                                success
+                                errorMessage
+                            }
+                        }
                         """, variable_values=dict(
             updateProjectStateMapsInput=dict(
                 projectKey=project_key,
@@ -348,3 +256,210 @@ class TestUpdateProjectStateMaps:
         assert 'errors' in response
         message = response['errors'][0]['message']
         assert 'got invalid value' in message
+
+
+@pytest.yield_fixture()
+def setup_work_items(setup_projects):
+    organization = setup_projects
+    project = organization.projects[0]
+    work_items_common = dict(
+
+        name='Issue 10',
+        display_id='1000',
+        created_at=get_date("2018-12-02"),
+        updated_at=get_date("2018-12-03"),
+        is_bug=True,
+        work_item_type='issue',
+        url='http://foo.com',
+        tags=['ares2'],
+        description='foo',
+        source_id=str(uuid.uuid4()),
+    )
+    # open state_type of this new work item should be updated to complete after the test
+    new_work_items = [
+        dict(
+            key=uuid.uuid4().hex,
+            **work_items_common,
+            state='todo'
+        ),
+        dict(
+            key=uuid.uuid4().hex,
+            **work_items_common,
+            state='doing'
+        ),
+        dict(
+            key=uuid.uuid4().hex,
+            **work_items_common,
+            state='done'
+        )
+    ]
+
+    with db.orm_session() as session:
+        session.expire_on_commit = False
+        session.add(organization)
+        session.add(project)
+        project.work_items_sources.append(
+            WorkItemsSource(
+                organization_key=str(organization.key),
+                organization_id=organization.id,
+                key=str(uuid.uuid4()),
+                name='foo',
+                integration_type='jira',
+                work_items_source_type='repository_issues',
+                commit_mapping_scope='repository',
+                source_id=str(uuid.uuid4())
+            )
+        )
+        project.work_items_sources[0].work_items.extend([
+            WorkItem(**item)
+            for item in new_work_items
+        ])
+    yield project
+
+
+class TestUpdateComputedWorkItemsStateTypes:
+
+    def it_updates_computed_state_types_when_a_state_map_is_initialized(self, setup_work_items):
+        client = Client(schema)
+        project = setup_work_items
+        project_key = str(project.key)
+        work_items_source_key = project.work_items_sources[0].key
+        work_item_key = project.work_items_sources[0].work_items[0].key
+        response = client.execute("""
+            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
+                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
+                        success
+                    }
+                }
+        """, variable_values=dict(
+            updateProjectStateMapsInput=dict(
+                projectKey=project_key,
+                workItemsSourceStateMaps=[
+                    dict(
+                        workItemsSourceKey=work_items_source_key,
+                        stateMaps=[
+                            dict(state="todo", stateType=WorkItemsStateType.open.value),
+                            dict(state="doing", stateType=WorkItemsStateType.wip.value),
+                            dict(state="done", stateType=WorkItemsStateType.complete.value)
+                        ]
+                    )
+                ]
+            )
+        )
+                                  )
+        assert 'data' in response
+        result = response['data']['updateProjectStateMaps']
+        assert result
+        assert result['success']
+
+        work_items_result = db.connection().execute(f"select state, state_type from analytics.work_items "
+                                f"inner join analytics.work_items_sources "
+                                f"on work_items.work_items_source_id = work_items_sources.id "
+                                f"where work_items_sources.key = '{work_items_source_key}'").fetchall()
+
+        assert len(work_items_result) == 3
+        assert set([
+            (work_item.state, work_item.state_type)
+            for work_item in work_items_result
+        ]) == {
+            ('todo', WorkItemsStateType.open.value),
+            ('doing', WorkItemsStateType.wip.value),
+            ('done', WorkItemsStateType.complete.value)
+        }
+
+    def it_updates_computed_state_types_after_initial_update(self, setup_work_items):
+        client = Client(schema)
+        project = setup_work_items
+        project_key = str(project.key)
+        work_items_source_key = project.work_items_sources[0].key
+        work_item_key = project.work_items_sources[0].work_items[0].key
+        response = client.execute("""
+            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
+                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
+                        success
+                    }
+                }
+        """, variable_values=dict(
+            updateProjectStateMapsInput=dict(
+                projectKey=project_key,
+                workItemsSourceStateMaps=[
+                    dict(
+                        workItemsSourceKey=work_items_source_key,
+                        stateMaps=[
+                            # making a nonsensical assignment here to make sure the update takes
+                            dict(state="todo", stateType=WorkItemsStateType.wip.value),
+                            dict(state="doing", stateType=WorkItemsStateType.complete.value),
+                            dict(state="done", stateType=WorkItemsStateType.open.value)
+                        ]
+                    )
+                ]
+            )
+        )
+                                  )
+        assert 'data' in response
+        result = response['data']['updateProjectStateMaps']
+        assert result
+        assert result['success']
+
+        work_items_result = db.connection().execute(f"select state, state_type from analytics.work_items "
+                                f"inner join analytics.work_items_sources "
+                                f"on work_items.work_items_source_id = work_items_sources.id "
+                                f"where work_items_sources.key = '{work_items_source_key}'").fetchall()
+
+        assert len(work_items_result) == 3
+        assert set([
+            (work_item.state, work_item.state_type)
+            for work_item in work_items_result
+        ]) == {
+            ('todo', WorkItemsStateType.wip.value),
+            ('doing', WorkItemsStateType.complete.value),
+            ('done', WorkItemsStateType.open.value)
+        }
+
+    def it_resets_unmapped_entries_to_null(self, setup_work_items):
+        client = Client(schema)
+        project = setup_work_items
+        project_key = str(project.key)
+        work_items_source_key = project.work_items_sources[0].key
+        work_item_key = project.work_items_sources[0].work_items[0].key
+        response = client.execute("""
+            mutation updateProjectStateMaps($updateProjectStateMapsInput: UpdateProjectStateMapsInput!) {
+                            updateProjectStateMaps(updateProjectStateMapsInput:$updateProjectStateMapsInput) {
+                        success
+                    }
+                }
+        """, variable_values=dict(
+            updateProjectStateMapsInput=dict(
+                projectKey=project_key,
+                workItemsSourceStateMaps=[
+                    dict(
+                        workItemsSourceKey=work_items_source_key,
+                        stateMaps=[
+                            # Leaving out the doing state from the mapping
+                            dict(state="todo", stateType=WorkItemsStateType.open.value),
+                            dict(state="done", stateType=WorkItemsStateType.complete.value)
+                        ]
+                    )
+                ]
+            )
+        )
+                                  )
+        assert 'data' in response
+        result = response['data']['updateProjectStateMaps']
+        assert result
+        assert result['success']
+
+        work_items_result = db.connection().execute(f"select state, state_type from analytics.work_items "
+                                                    f"inner join analytics.work_items_sources "
+                                                    f"on work_items.work_items_source_id = work_items_sources.id "
+                                                    f"where work_items_sources.key = '{work_items_source_key}'").fetchall()
+
+        assert len(work_items_result) == 3
+        assert set([
+            (work_item.state, work_item.state_type)
+            for work_item in work_items_result
+        ]) == {
+                   ('todo', WorkItemsStateType.open.value),
+                   ('doing', None),
+                   ('done', WorkItemsStateType.complete.value)
+               }

765e905dea22521f984ab21d111a92ff38aadfeb
Maneet Sehgal <maneet@64sqs.com>
2020-03-18 21:55:56 +0530

    PO-82: WIP: Added logic to recreate delivery cycles

--- polaris/analytics/db/impl/projects.py
+++ polaris/analytics/db/impl/projects.py
@@ -9,18 +9,20 @@
 # Author: Krishna Kumar
 import logging
 
-from sqlalchemy.sql.expression import and_
+from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.sql.expression import and_, select, extract
 
 from polaris.analytics.db.enums import WorkItemsStateType
 from polaris.analytics.db.model import Project, WorkItemsSource, work_items, work_items_source_state_map, WorkItem, \
-    work_item_delivery_cycles, work_items_sources
+    work_item_delivery_cycles, work_items_sources, work_item_state_transitions
 from polaris.utils.collections import find
 from polaris.utils.exceptions import ProcessingException
 
 logger = logging.getLogger('polaris.analytics.db.impl')
 
 
-def update_work_items_computed_state_types(session, work_items_source_id, update_delivery_cycle):
+def update_work_items_computed_state_types(session, work_items_source_id, update_delivery_cycle, new_closed_state):
     logger.info('--update_delivery_cycle-------- ' + str(update_delivery_cycle))
     logger.info('----' + str(work_items_source_id))
     updated = session.execute(
@@ -65,19 +67,83 @@ def update_work_items_computed_state_types(session, work_items_source_id, update
         )
 
         #insert new delivery cycles
+        session.execute(
+            insert(work_item_delivery_cycles).from_select(
+                [
+                    'work_item_id',
+                    'start_seq_no',
+                    'start_date'
+                ],
+                select([
+                    work_item_state_transitions.c.work_item_id,
+                    work_item_state_transitions.c.seq_no.label('start_seq_no'),
+                    work_item_state_transitions.c.created_at.label('start_date')
+                ]).select_from(
+                    work_item_state_transitions.join(
+                        work_items, work_items.c.id == work_item_state_transitions.c.work_item_id
+                    )
+                ).where (
+                    and_(
+                        work_item_state_transitions.c.seq_no == 0,
+                        work_items.c.work_items_source_id == work_items_source_id
+                    )
+                )
+            )
+        )
 
+        session.execute(
+            insert(work_item_delivery_cycles).from_select(
+                [
+                    'work_item_id',
+                    'start_seq_no',
+                    'start_date'
+                ],
+                select([
+                    work_item_state_transitions.c.work_item_id,
+                    work_item_state_transitions.c.seq_no.label('start_seq_no'),
+                    work_item_state_transitions.c.created_at.label('start_date')
+                ]).select_from(
+                    work_item_state_transitions.join(
+                        work_items, work_items.c.id == work_item_state_transitions.c.work_item_id
+                    )
+                ).where(
+                    and_(
+                        work_item_state_transitions.c.previous_state == new_closed_state.state,
+                        work_items.c.work_items_source_id == work_items_source_id
+                    )
+                )
+            )
+        )
 
+        # update delivery cycles for work_items transitioning to losed state_type
+        session.execute(
+            work_item_delivery_cycles.update().values(
+                end_seq_no=work_item_state_transitions.c.seq_no,
+                end_date=work_item_state_transitions.c.created_at,
+                lead_time=func.trunc((extract('epoch', work_item_state_transitions.c.created_at) - \
+                                      extract('epoch', work_item_delivery_cycles.c.start_date)))
+            ).select_from(
+                work_item_state_transitions.join(
+                    work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == work_item_state_transitions.c.work_item_id,
+                )
+            ).where(
+                and_(
+                    work_item_delivery_cycles.c.state == new_closed_state.state,
+                    work_item_delivery_cycles.c.start_date < work_item_state_transitions.c.created_at
+                )
+            )
+        )
     return updated
 
 
-def update_work_items_source_state_mapping(session, work_items_source_key, state_mappings, update_delivery_cycle):
+def update_work_items_source_state_mapping(session, work_items_source_key, state_mappings, update_delivery_cycle, new_closed_state):
     logger.info('*************')
     work_items_source = WorkItemsSource.find_by_work_items_source_key(session, work_items_source_key)
     if work_items_source is not None:
         work_items_source.init_state_map(state_mappings)
         session.flush()
         logger.info('*************')
-        update_work_items_computed_state_types(session, work_items_source.id, update_delivery_cycle)
+        update_work_items_computed_state_types(session, work_items_source.id, update_delivery_cycle, new_closed_state)
 
 def update_project_work_items_source_state_mappings(session, project_state_maps):
     logger.info("Inside update_project_work_items_state_mappings")
@@ -89,6 +155,7 @@ def update_project_work_items_source_state_mappings(session, project_state_maps)
         for work_items_source_map in project_state_maps.work_items_source_state_maps:
             source_key = work_items_source_map.work_items_source_key
             closed = [i for i, state_map in enumerate(work_items_source_map.state_maps) if state_map.state_type == WorkItemsStateType.closed.value]
+            logger.info("-----------------------")
             if len(closed) > 1:
                 raise ProcessingException(f'Work Items Source can have only one closed state')
             else:
@@ -99,13 +166,14 @@ def update_project_work_items_source_state_mappings(session, project_state_maps)
                                             lambda w: str(w.state_type) == str(WorkItemsStateType.closed.value))
                     new_closed_state = find(work_items_source_map.state_maps,
                                             lambda w: str(w.state_type) == str(WorkItemsStateType.closed.value))
-                    logger.info('-----old_closed_state----' + str(old_closed_state.state))
-                    logger.info('-----new_closed_state----' + str(new_closed_state.state))
+                    logger.info("-----------------------++++" + str(work_item_source.state_maps))
+                    logger.info('-----old_closed_state----' + str(old_closed_state))
+                    logger.info('-----new_closed_state----' + str(new_closed_state))
                     if old_closed_state.state != new_closed_state.state:
-                        update_work_items_source_state_mapping(session, source_key, work_items_source_map.state_maps, True)
+                        update_work_items_source_state_mapping(session, source_key, work_items_source_map.state_maps, True, new_closed_state)
                     else:
                         update_work_items_source_state_mapping(session, source_key, work_items_source_map.state_maps,
-                                                               False)
+                                                               False, new_closed_state)
                     updated.append(source_key)
 
                 else:

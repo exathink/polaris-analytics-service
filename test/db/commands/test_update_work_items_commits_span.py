fc8098fb37cd570e7af4ef5e7d90a0abd8e3d411
Pragya Goyal <pragyagoyal@gmail.com>
2020-03-30 22:12:24 +0530

    PO-88 added more tests for command

--- test/db/commands/test_update_work_items_commits_span.py
+++ test/db/commands/test_update_work_items_commits_span.py
@@ -46,3 +46,106 @@ class TestUpateWorkItemsCommitsSpan:
         assert db.connection().execute(
             f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
                     work_item_id={work_items_ids[0]} and earliest_commit is not NULL").scalar() == 2
+        assert db.connection().execute(f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                    work_item_id={work_items_ids[0]} and earliest_commit=latest_commit").scalar() == 1
+
+    def it_updates_commit_spans_for_multiple_work_items(self, update_work_items_commits_span_fixture):
+        organization, work_items_ids, test_commits, test_work_items = update_work_items_commits_span_fixture
+        work_items_commits = [
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[0]['key'],
+                commit_key=test_commits[0]['key']
+            ),
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[1]['key'],
+                commit_key=test_commits[4]['key']
+            )
+        ]
+        result = commands.update_work_items_commits_span(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 3
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                            work_item_id={work_items_ids[0]} and earliest_commit is not NULL").scalar() == 2
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[1]} and earliest_commit is not NULL and earliest_commit=latest_commit").scalar() == 1
+
+    def it_doesnt_update_commit_spans_for_work_items_not_in_input(self, update_work_items_commits_span_fixture):
+        organization, work_items_ids, test_commits, test_work_items = update_work_items_commits_span_fixture
+        work_items_commits = [
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[0]['key'],
+                commit_key=test_commits[0]['key']
+            )
+        ]
+        result = commands.update_work_items_commits_span(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 2
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                            work_item_id={work_items_ids[1]} and earliest_commit is NULL and latest_commit is NULL").scalar() == 1
+
+    def it_is_idempotent(self, update_work_items_commits_span_fixture):
+        organization, work_items_ids, test_commits, test_work_items = update_work_items_commits_span_fixture
+        work_items_commits = [
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[0]['key'],
+                commit_key=test_commits[0]['key']
+            ),
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[1]['key'],
+                commit_key=test_commits[4]['key']
+            )
+        ]
+        result = commands.update_work_items_commits_span(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 3
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[0]} and earliest_commit is not NULL").scalar() == 2
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[1]} and earliest_commit is not NULL and earliest_commit=latest_commit").scalar() == 1
+
+        # call again
+        result = commands.update_work_items_commits_span(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 3
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                            work_item_id={work_items_ids[0]} and earliest_commit is not NULL").scalar() == 2
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[1]} and earliest_commit is not NULL and earliest_commit=latest_commit").scalar() == 1
+
+    def it_updates_commit_spans_when_a_commit_is_mapped_to_more_than_one_work_item(self, update_work_items_commits_span_fixture):
+        organization, work_items_ids, test_commits, test_work_items = update_work_items_commits_span_fixture
+
+        # Map commit 1 to work item 2
+        create_work_item_commits(test_work_items[1]['key'], [test_commits[1]['key']])
+        work_items_commits = [
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[0]['key'],
+                commit_key=test_commits[0]['key']
+            ),
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[1]['key'],
+                commit_key=test_commits[4]['key']
+            )
+        ]
+        result = commands.update_work_items_commits_span(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 3
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[0]} and earliest_commit is not NULL").scalar() == 2
+        assert db.connection().execute(f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[1]} and earliest_commit is not NULL and latest_commit>earliest_commit").scalar() == 1

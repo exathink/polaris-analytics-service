eebbc2ad72f3ae35f9ed81beb67cf2ebab140f2f
Pragya Goyal <pragyagoyal@gmail.com>
2020-03-31 22:39:05 +0530

    PO-98 Moved common code to compute_implementation_complexity_metrics

--- polaris/analytics/db/impl/work_tracking.py
+++ polaris/analytics/db/impl/work_tracking.py
@@ -1250,9 +1250,53 @@ def infer_projects_repositories_relationships(session, organization_key, work_it
     )
 
 
-def update_work_items_commits_span(session, organization_key, work_items_commits):
+def update_work_items_commits_span(session, work_items_temp):
     # Update work items in work_items_commits_span with earliest and latest commits
     updated = 0
+
+    # select relevant rows to find commits span
+    delivery_cycles_commits_rows = select([
+        work_item_delivery_cycles.c.delivery_cycle_id.label('delivery_cycle_id'),
+        func.min(commits.c.commit_date).label('earliest_commit'),
+        func.max(commits.c.commit_date).label('latest_commit')
+    ]).select_from(
+        work_items_temp.join(
+            work_items, work_items.c.key == work_items_temp.c.work_item_key
+        ).join(
+            work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == work_items.c.id
+        ).join(
+            work_items_commits_table, work_items_commits_table.c.work_item_id == work_items.c.id
+        ).join(
+            commits, work_items_commits_table.c.commit_id == commits.c.id
+        )
+    ).where(
+        commits.c.commit_date >= work_item_delivery_cycles.c.start_date
+    ).group_by(
+        work_item_delivery_cycles.c.delivery_cycle_id,
+
+    ).cte('delivery_cycles_commits_rows')
+
+    # update relevant work items delivery cycles with earliest and latest commit dates
+    updated = session.connection().execute(
+        work_item_delivery_cycles.update().where(
+            work_item_delivery_cycles.c.delivery_cycle_id == delivery_cycles_commits_rows.c.delivery_cycle_id
+        ).values(
+            earliest_commit=delivery_cycles_commits_rows.c.earliest_commit,
+            latest_commit=delivery_cycles_commits_rows.c.latest_commit
+        )
+    ).rowcount
+
+    return updated
+
+
+def update_work_items_commits_repository_count(session, work_items_temp):
+    updated = 0
+    return updated
+
+
+def compute_implementation_complexity_metrics(session, organization_key, work_items_commits):
+    updated_commits_span = 0
+    updated_commits_repository_count = 0
 
     if len(work_items_commits) > 0:
         # create a temp table for received work item ids
@@ -1281,48 +1325,8 @@ def update_work_items_commits_span(session, organization_key, work_items_commits
             )
         )
 
-        # select relevant rows to find commits span
-        delivery_cycles_commits_rows = select([
-            work_item_delivery_cycles.c.delivery_cycle_id.label('delivery_cycle_id'),
-            func.min(commits.c.commit_date).label('earliest_commit'),
-            func.max(commits.c.commit_date).label('latest_commit')
-        ]).select_from(
-            work_items_temp.join(
-                work_items, work_items.c.key == work_items_temp.c.work_item_key
-            ).join(
-                work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == work_items.c.id
-            ).join(
-                work_items_commits_table, work_items_commits_table.c.work_item_id == work_items.c.id
-            ).join(
-                commits, work_items_commits_table.c.commit_id == commits.c.id
-            )
-        ).where(
-            commits.c.commit_date >= work_item_delivery_cycles.c.start_date
-        ).group_by(
-            work_item_delivery_cycles.c.delivery_cycle_id,
-
-        ).cte('delivery_cycles_commits_rows')
-
-        # update relevant work items delivery cycles with earliest and latest commit dates
-        updated = session.connection().execute(
-            work_item_delivery_cycles.update().where(
-                work_item_delivery_cycles.c.delivery_cycle_id == delivery_cycles_commits_rows.c.delivery_cycle_id
-            ).values(
-                earliest_commit=delivery_cycles_commits_rows.c.earliest_commit,
-                latest_commit=delivery_cycles_commits_rows.c.latest_commit
-            )
-        ).rowcount
-
-    return updated
-
-
-def update_work_items_commits_repository_count(session, organization_key, work_items_commits):
-    return 0
-
-
-def compute_implementation_complexity_metrics(session, organization_key, work_items_commits):
-    updated_commits_span = update_work_items_commits_span(session, organization_key, work_items_commits)
-    updated_commits_repository_count = update_work_items_commits_repository_count(session, organization_key, work_items_commits)
+        updated_commits_span = update_work_items_commits_span(session, work_items_temp)
+        updated_commits_repository_count = update_work_items_commits_repository_count(session, work_items_temp)
     return dict(
         updated_commits_span=updated_commits_span,
         updated_commits_repository_count=updated_commits_repository_count

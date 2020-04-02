698fd762a575bf8cd32a47c44a8f65e58713e620
Pragya Goyal <pragyagoyal@gmail.com>
2020-04-02 16:53:16 +0530

    PO-98 one more test added

--- test/db/commands/test_compute_implementation_complexity_metrics.py
+++ test/db/commands/test_compute_implementation_complexity_metrics.py
@@ -31,7 +31,7 @@ class TestUpateWorkItemsCommitsSpan:
             f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
             work_item_id={work_items_ids[1]} and earliest_commit is not NULL").scalar() == 1
 
-    def it_updates_commit_spans_for_work_item_with_multiple_delivery_cycles(self, work_items_commits_fixture):
+    def it_updates_commits_span_for_work_item_with_multiple_delivery_cycles(self, work_items_commits_fixture):
         organization, work_items_ids, test_commits, test_work_items = work_items_commits_fixture
         work_items_commits = [
             dict(
@@ -49,7 +49,7 @@ class TestUpateWorkItemsCommitsSpan:
         assert db.connection().execute(f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
                     work_item_id={work_items_ids[0]} and earliest_commit=latest_commit").scalar() == 1
 
-    def it_updates_commit_spans_for_multiple_work_items(self, work_items_commits_fixture):
+    def it_updates_commits_span_for_multiple_work_items(self, work_items_commits_fixture):
         organization, work_items_ids, test_commits, test_work_items = work_items_commits_fixture
         work_items_commits = [
             dict(
@@ -73,7 +73,7 @@ class TestUpateWorkItemsCommitsSpan:
             f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
                                     work_item_id={work_items_ids[1]} and earliest_commit is not NULL and earliest_commit=latest_commit").scalar() == 1
 
-    def it_doesnt_update_commit_spans_for_work_items_not_in_input(self, work_items_commits_fixture):
+    def it_doesnt_update_commits_span_for_work_items_not_in_input(self, work_items_commits_fixture):
         organization, work_items_ids, test_commits, test_work_items = work_items_commits_fixture
         work_items_commits = [
             dict(
@@ -124,10 +124,10 @@ class TestUpateWorkItemsCommitsSpan:
             f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
                                     work_item_id={work_items_ids[1]} and earliest_commit is not NULL and earliest_commit=latest_commit").scalar() == 1
 
-    def it_updates_commit_spans_when_a_commit_is_mapped_to_more_than_one_work_item(self, work_items_commits_fixture):
+    def it_updates_commits_span_when_a_commit_is_mapped_to_more_than_one_work_item(self, work_items_commits_fixture):
         organization, work_items_ids, test_commits, test_work_items = work_items_commits_fixture
 
-        # Map commit 1 to work item 2
+        # Map commit 2 to work item 2 resulting in different earliest and latest commit dates
         create_work_item_commits(test_work_items[1]['key'], [test_commits[1]['key']])
         work_items_commits = [
             dict(
@@ -255,3 +255,32 @@ class TestUpdateWorkItemsCommitsRepositoryCount:
         assert db.connection().execute(
             f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
                                             work_item_id={work_items_ids[0]} and repository_count is not NULL").scalar() == 2
+
+    def it_updates_repository_count_when_a_commit_is_mapped_to_more_than_one_work_item(self, work_items_commits_fixture):
+        organization, work_items_ids, test_commits, test_work_items = work_items_commits_fixture
+
+        # Map commit 2 to work item 2 resulting in increase in repo count
+        create_work_item_commits(test_work_items[1]['key'], [test_commits[1]['key']])
+        work_items_commits = [
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[0]['key'],
+                commit_key=test_commits[0]['key']
+            ),
+            dict(
+                organization_key=organization.key,
+                work_item_key=test_work_items[1]['key'],
+                commit_key=test_commits[4]['key']
+            )
+        ]
+        result = commands.compute_implementation_complexity_metrics(organization.key, work_items_commits)
+        assert result['success']
+        assert result['updated'] == 3
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[0]} and repository_count=2").scalar() == 1
+        assert db.connection().execute(
+            f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                            work_item_id={work_items_ids[0]} and repository_count=1").scalar() == 1
+        assert db.connection().execute(f"select count(delivery_cycle_id) from analytics.work_item_delivery_cycles where \
+                                    work_item_id={work_items_ids[1]} and repository_count=2").scalar() == 1

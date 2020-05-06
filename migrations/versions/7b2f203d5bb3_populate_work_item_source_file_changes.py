7c9c3386da502cb1d39fb1e76359dae674c958e4
Pragya Goyal <pragyagoyal@gmail.com>
2020-05-06 18:38:23 +0530

    Migration fixed

--- migrations/versions/7b2f203d5bb3_populate_work_item_source_file_changes.py
+++ migrations/versions/7b2f203d5bb3_populate_work_item_source_file_changes.py
@@ -16,17 +16,13 @@ depends_on = None
 
 
 def upgrade():
-    # insert into work_item_source_file_changes
+    # First pass: Inserts all work item commits within an existing delivery cycle
     op.execute("""
         WITH source_files_details AS
-         (SELECT work_items.id                as                 work_item_id,
-                 (CASE
-                        WHEN (commits.commit_date >= work_item_delivery_cycles.start_date 
-                        AND commits.commit_date <= work_item_delivery_cycles.end_date)
-                        THEN work_item_delivery_cycles.delivery_cycle_id
-                        ELSE NULL END) as delivery_cycle_id,
-                 work_items_commits.commit_id as                 commit_id,
-                 source_files.id              as                 source_file_id,
+         (SELECT work_items.id                                   as work_item_id,
+                 work_item_delivery_cycles.delivery_cycle_id     as delivery_cycle_id,
+                 work_items_commits.commit_id                    as commit_id,
+                 source_files.id                                 as source_file_id,
                  analytics.source_files.repository_id,
                  commit_date,
                  source_commit_id,
@@ -34,42 +30,89 @@ def upgrade():
                  author_contributor_alias_id,
                  num_parents,
                  created_on_branch,
-                 sf ->> 'action' as                              file_action,
-                 CAST(sf -> 'stats' ->> 'lines' AS INTEGER) as     total_lines_changed,
-                 CAST(sf -> 'stats' ->> 'deletions' As INTEGER) as total_lines_deleted,
+                 sf ->> 'action'                                 as file_action,
+                 CAST(sf -> 'stats' ->> 'lines' AS INTEGER)      as total_lines_changed,
+                 CAST(sf -> 'stats' ->> 'deletions' As INTEGER)  as total_lines_deleted,
                  CAST(sf -> 'stats' ->> 'insertions' AS INTEGER) as total_lines_added
-          FROM analytics.source_files,
-               analytics.work_items,
-               analytics.commits,
-               analytics.work_items_commits,
-               analytics.work_item_delivery_cycles,
+          FROM analytics.commits
+                   join analytics.work_items_commits on commits.id = work_items_commits.commit_id
+                   join analytics.work_items on work_items_commits.work_item_id = work_items.id
+                   join analytics.work_item_delivery_cycles on work_items.id = work_item_delivery_cycles.work_item_id,
                jsonb_array_elements(analytics.commits.source_files) sf
-          where UUID(sf ->> 'key') = source_files.key
-            AND work_items.id = work_items_commits.work_item_id
-            AND work_items_commits.commit_id = commits.id
-            AND work_items.id = work_item_delivery_cycles.work_item_id
-            AND commits.num_parents = 1)
-        INSERT
-        INTO analytics.work_item_source_file_changes(work_item_id, delivery_cycle_id, repository_id, source_file_id, commit_id,
+                   join analytics.source_files on UUID(sf ->> 'key') = source_files.key
+          where commits.commit_date >= work_item_delivery_cycles.start_date
+            AND (work_item_delivery_cycles.end_date is NULL OR
+                 work_item_delivery_cycles.end_date >= commits.commit_date))
+            INSERT
+            INTO analytics.work_item_source_file_changes(work_item_id, delivery_cycle_id, repository_id, source_file_id, commit_id,
+                                                 source_commit_id, commit_date, committer_contributor_alias_id,
+                                                 author_contributor_alias_id, num_parents, created_on_branch, file_action,
+                                                 total_lines_changed, total_lines_deleted, total_lines_added)
+            SELECT work_item_id,
+                delivery_cycle_id,
+                repository_id,
+                source_file_id,
+                commit_id,
+                source_commit_id,
+                commit_date,
+                committer_contributor_alias_id,
+                author_contributor_alias_id,
+                num_parents,
+                created_on_branch,
+                file_action,
+                total_lines_changed,
+                total_lines_deleted,
+                total_lines_added
+            from source_files_details""")
+
+    # Second pass: Inserting those work item commit source files which fall out of all delivery cycles
+    op.execute("""
+    WITH source_files_details AS
+         (SELECT work_items.id                                   as work_item_id,
+                 work_items_commits.commit_id                    as commit_id,
+                 source_files.id                                 as source_file_id,
+                 analytics.source_files.repository_id,
+                 commits.commit_date,
+                 commits.source_commit_id,
+                 commits.committer_contributor_alias_id,
+                 commits.author_contributor_alias_id,
+                 commits.num_parents,
+                 commits.created_on_branch,
+                 sf ->> 'action'                                 as file_action,
+                 CAST(sf -> 'stats' ->> 'lines' AS INTEGER)      as total_lines_changed,
+                 CAST(sf -> 'stats' ->> 'deletions' As INTEGER)  as total_lines_deleted,
+                 CAST(sf -> 'stats' ->> 'insertions' AS INTEGER) as total_lines_added
+          FROM analytics.commits
+                   join analytics.work_items_commits on commits.id = work_items_commits.commit_id
+                   join analytics.work_items on work_items_commits.work_item_id = work_items.id
+                   join analytics.work_item_delivery_cycles on work_items.id = work_item_delivery_cycles.work_item_id
+                   full outer join analytics.work_item_source_file_changes on
+                  work_item_source_file_changes.commit_id = commits.id and
+                  work_item_source_file_changes.work_item_id = work_items.id,
+               jsonb_array_elements(analytics.commits.source_files) sf
+                   join analytics.source_files on UUID(sf ->> 'key') = source_files.key
+          where work_item_source_file_changes.commit_id is NULL)
+    INSERT
+    INTO analytics.work_item_source_file_changes(work_item_id, delivery_cycle_id, repository_id, source_file_id, commit_id,
                                              source_commit_id, commit_date, committer_contributor_alias_id,
                                              author_contributor_alias_id, num_parents, created_on_branch, file_action,
                                              total_lines_changed, total_lines_deleted, total_lines_added)
-        SELECT work_item_id,
-            delivery_cycle_id,
-            repository_id,
-            source_file_id,
-            commit_id,
-            source_commit_id,
-            commit_date,
-            committer_contributor_alias_id,
-            author_contributor_alias_id,
-            num_parents,
-            created_on_branch,
-            file_action,
-            total_lines_changed,
-            total_lines_deleted,
-            total_lines_added
-        from source_files_details""")
+    SELECT work_item_id,
+       NULL,
+       repository_id,
+       source_file_id,
+       commit_id,
+       source_commit_id,
+       commit_date,
+       committer_contributor_alias_id,
+       author_contributor_alias_id,
+       num_parents,
+       created_on_branch,
+       file_action,
+       total_lines_changed,
+       total_lines_deleted,
+       total_lines_added
+    from source_files_details""")
 
 
 def downgrade():

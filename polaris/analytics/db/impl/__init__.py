8db6c1dde2af9bcca3f67c7d1e667d61d759944f
Pragya Goyal <pragyagoyal@gmail.com>
2020-05-07 18:48:28 +0530

    Split impl/work_tracking into 2 files

--- polaris/analytics/db/impl/__init__.py
+++ polaris/analytics/db/impl/__init__.py
@@ -1,6 +1,7 @@
 from .commit_import import *
 from .source_file_import import *
 from .work_tracking import *
+from .delivery_cycle_tracking import *
 from .contributors import *
 from .repositories import *
 from .projects import *

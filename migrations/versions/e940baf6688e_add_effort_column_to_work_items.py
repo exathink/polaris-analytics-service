3337a278cf8c9bd24579e8c688c1ffa7251fa8c8
Krishna Kumar <kkumar@exathink.com>
2020-08-23 14:43:28 -0500

    Remove effort field from delivery cycles in model and migrations. We are going to add that back when implement effort calculations at the delivery cycle level.
    [story=PO-263  story_url=https://urjuna.atlassian.net/browse/PO-263]

--- migrations/versions/e940baf6688e_add_effort_column_to_work_items.py
+++ migrations/versions/e940baf6688e_add_effort_column_to_work_items.py
@@ -1,4 +1,4 @@
-"""add_effort_column_to_work_items_n_delivery_cycles
+"""add_effort_column_to_work_items
 
 Revision ID: e940baf6688e
 Revises: 69c93b8aa4e7
@@ -17,12 +17,11 @@ depends_on = None
 
 
 def upgrade():
-    op.add_column('work_item_delivery_cycles', sa.Column('effort', sa.Float(), nullable=True), schema='analytics')
     op.add_column('work_items', sa.Column('effort', sa.Float(), nullable=True), schema='analytics')
 
 
 
 def downgrade():
     op.drop_column('work_items', 'effort', schema='analytics')
-    op.drop_column('work_item_delivery_cycles', 'effort', schema='analytics')
+
 

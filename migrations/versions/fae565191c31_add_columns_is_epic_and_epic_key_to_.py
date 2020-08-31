2574df0b341b6a0b6de458d5a9aaa927fd3c4ea4
Pragya Goyal <pragya@64sqs.com>
2020-08-31 17:00:28 +0530

    updated migration with constraint name and update query for is_epic column

--- migrations/versions/fae565191c31_add_columns_is_epic_and_epic_key_to_.py
+++ migrations/versions/fae565191c31_add_columns_is_epic_and_epic_key_to_.py
@@ -8,7 +8,6 @@ Create Date: 2020-08-24 15:34:37.582030
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision = 'fae565191c31'
 down_revision = 'e940baf6688e'
@@ -16,13 +15,22 @@ branch_labels = None
 depends_on = None
 
 
+def update_is_epic():
+    op.execute(f"""
+                update analytics.work_items set is_epic=TRUE where work_item_type='epic'   
+    """)
+
+
 def upgrade():
     op.add_column('work_items', sa.Column('epic_id', sa.Integer(), nullable=True), schema='analytics')
-    op.add_column('work_items', sa.Column('is_epic', sa.Boolean(), server_default='FALSE', nullable=False), schema='analytics')
-    op.create_foreign_key('epic_issue_relationship', 'work_items', 'work_items', ['epic_id'], ['id'], source_schema='analytics', referent_schema='analytics')
+    op.add_column('work_items', sa.Column('is_epic', sa.Boolean(), server_default='FALSE', nullable=False),
+                  schema='analytics')
+    op.create_foreign_key('work_items_epic_id_fk', 'work_items', 'work_items', ['epic_id'], ['id'],
+                          source_schema='analytics', referent_schema='analytics')
+    update_is_epic()
 
 
 def downgrade():
-    op.drop_constraint('epic_issue_relationship', 'work_items', schema='analytics', type_='foreignkey')
+    op.drop_constraint('work_items_epic_id_fk', 'work_items', schema='analytics', type_='foreignkey')
     op.drop_column('work_items', 'is_epic', schema='analytics')
     op.drop_column('work_items', 'epic_id', schema='analytics')

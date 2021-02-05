c9f1be9e9b2c78f0926ce0d1c6f6f0f3d137456a
Pragya Goyal <pragya@64sqs.com>
2021-02-05 21:59:18 +0530

    Added basic tests using new format or tests and fixtures using api helper class

--- test/graphql/queries/test_accounts.py
+++ test/graphql/queries/test_accounts.py
@@ -14,9 +14,11 @@ from test.fixtures.graphql import *
 from test.constants import *
 from unittest.mock import patch
 from polaris.common import db
-
+from sqlalchemy import and_
 from graphene.test import Client
 from polaris.analytics.service.graphql import schema
+from polaris.analytics.db.model import Contributor, ContributorAlias, repositories_contributor_aliases
+
 
 class TestAccount:
 
@@ -33,7 +35,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
         assert 'data' in response
         result = response['data']['account']
@@ -64,7 +66,6 @@ class TestAccount:
         assert result['created']
         assert result['updated']
 
-
     def it_implements_the_owner_info_interface(self, org_repo_fixture):
         client = Client(schema)
 
@@ -85,8 +86,6 @@ class TestAccount:
         assert result
         assert result['ownerKey'] == str(uuid.UUID(test_user_key))
 
-
-
     def it_resolves_organizations(self, org_repo_fixture):
         client = Client(schema)
 
@@ -106,7 +105,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
         assert 'data' in response
         result = response['data']['account']
@@ -135,7 +134,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
         assert 'data' in response
         result = response['data']['account']
@@ -144,8 +143,6 @@ class TestAccount:
         assert len(organizations) == 1
         assert organizations[0]['node']['projectCount'] == 2
         assert organizations[0]['node']['repositoryCount'] == 4
-
-
 
     def it_resolves_projects(self, org_repo_fixture):
         client = Client(schema)
@@ -166,7 +163,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
         assert 'data' in response
         result = response['data']['account']
@@ -177,7 +174,6 @@ class TestAccount:
             assert project['node']['id']
             assert project['node']['key']
             assert project['node']['name']
-
 
     def it_resolves_project_repository_count(self, org_repo_fixture):
         client = Client(schema)
@@ -196,7 +192,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
         assert 'data' in response
         result = response['data']['account']
@@ -224,7 +220,7 @@ class TestAccount:
                             }
                         }
                     """, variable_values=dict(account_key=test_account_key)
-            )
+                                      )
 
             assert 'data' in response
             result = response['data']['account']
@@ -235,16 +231,242 @@ class TestAccount:
                 assert uuid.UUID(contributor['node']['key']).hex == test_contributor_key
 
 
+class ContributorImportApiHelper:
+
+    def __init__(self, repositories, contributors_info):
+        self.repositories = repositories
+        self.contributors_info = contributors_info
+
+    def import_contributors(self):
+        contributor_objects = []
+        contributor_alias_objects = []
+        with db.orm_session() as session:
+            for info in self.contributors_info:
+                contributor = Contributor(
+                        key=info['key'],
+                        name=info['name']
+                )
+                session.add(contributor)
+                session.flush()
+                contributor_objects.append(contributor)
+                contributor_alias = ContributorAlias(
+                        key=info['key'],
+                        name=info['name'],
+                        source_alias=info['source_alias'],
+                        contributor_id=contributor.id,
+                        source='vcs',
+                        robot=False
+                )
+                session.add(contributor_alias)
+                contributor_alias_objects.append(contributor_alias)
+
+        return contributor_objects, contributor_alias_objects
+
+    def update_contributor(self, contributor_key, update_dict):
+        pass
+
+    def update_contributor_alias(self, alias_key, update_dict):
+        pass
+
+    def add_repository_contributor_alias(self, mapping, join_this=None):
+        with db.orm_session(join_this) as session:
+            session.connection().execute(
+                repositories_contributor_aliases.insert(
+                    mapping
+            )
+        )
+
+    def update_repository_contributor_alias(self, repository_id, contributor_alias_id, updated_mapping, join_this=None):
+        with db.orm_session(join_this) as session:
+            session.connection().execute(
+                repositories_contributor_aliases.update().where(
+                    and_(
+                        repositories_contributor_aliases.c.repository_id==repository_id,
+                        repositories_contributor_aliases.c.contributor_alias_id==contributor_alias_id
+                    )
+                ).values(
+                    updated_mapping
+                )
+            )
+
+
 class TestAccountContributorsConnection:
 
-    def setup(self, account_contributor_with_aliases_setup):
-        pass
+    @pytest.yield_fixture()
+    def setup(self, org_repo_fixture):
+        organization, projects, repositories = org_repo_fixture
+        contributor_aliases_info = [
+            dict(
+                key=uuid.uuid4(),
+                name='Joe Blow',
+                source_alias='joe@blow.com'
+            ),
+            dict(
+                key=uuid.uuid4(),
+                name='Joe Blow',
+                source_alias='joe@local-macbook-pro'
+            ),
+            dict(
+                key=uuid.uuid4(),
+                name='Ida J',
+                source_alias='ida@jay.com'
+            )
+        ]
 
-    class TestWithoutFilterWithoutInterface:
-        pass
+        api_helper = ContributorImportApiHelper(repositories, contributor_aliases_info)
 
-    class TestWithFilterWithoutInterface:
-        pass
+        query = """
+                    query getAccountContributorNodes($account_key:String!) {
+                        account(key: $account_key) {
+                            contributors(interfaces:[CommitSummary, ContributorAliasesInfo], commitWithinDays:20){
+                                edges {
+                                    node {
+                                        id
+                                        key
+                                        name
+                                        earliestCommit
+                                        latestCommit
+                                        commitCount
+                                        contributorAliasesInfo {
+                                            key
+                                            name
+                                            alias
+                                            latestCommit
+                                            earliestCommit
+                                            commitCount
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                """
+
+        yield Fixture(
+            repositories=repositories,
+            contributors=contributors,
+            contributor_aliases=contributor_aliases,
+            api_helper=api_helper,
+            query=query
+        )
+
+    class TestWithNoContributors:
+
+        class TestWithoutFilterWithoutInterface:
+
+            @pytest.yield_fixture()
+            def setup(self, setup):
+                fixture = setup
+                plain_query = """
+                            query getAccountContributorNodes($account_key:String!) {
+                                account(key: $account_key) {
+                                    contributors{
+                                        edges {
+                                            node {
+                                                key
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        """
+                yield Fixture(
+                    parent=fixture,
+                    plain_query=plain_query
+                )
+
+            def it_returns_no_contributors_when_there_are_none(self, setup):
+                fixture = setup
+                client = Client(schema)
+
+                with patch('polaris.analytics.service.graphql.account.Account.check_access', return_value=True):
+                    response = client.execute(fixture.query, variable_values=dict(account_key=test_account_key))
+
+                    assert 'data' in response
+                    result = response['data']['account']
+                    assert len(result['contributors']['edges']) == 0
+
+        class TestWithFilterWithInterface:
+
+            def it_returns_no_contributors_when_there_are_none(self, setup):
+                fixture = setup
+                client = Client(schema)
+
+                with patch('polaris.analytics.service.graphql.account.Account.check_access', return_value=True):
+                    response = client.execute(fixture.query, variable_values=dict(account_key=test_account_key))
+
+                    assert 'data' in response
+                    result = response['data']['account']
+                    assert len(result['contributors']['edges']) == 0
+
+    class TestWithContributorsWithSingleAlias:
+
+        @pytest.yield_fixture()
+        def setup(self, setup):
+            fixture = setup
+            api_helper = fixture.api_helper
+            contributor_objects, contributor_alias_objects = api_helper.import_contributors()
+            # Add repository_contributor_aliases mappings
+            api_helper.add_repository_contributor_alias(
+                dict(
+                    repository_id=fixture.repositories['alpha'].id,
+                    contributor_alias_id=contributor_alias_objects[0].id,
+                    earliest_commit=get_date("2018-12-03"),
+                    latest_commit=datetime.utcnow() - timedelta(days=10),
+                    commit_count=200,
+                    contributor_id=contributor_objects[0].id,
+                    robot=False
+                )
+            )
+            api_helper.add_repository_contributor_alias(
+                dict(
+                    repository_id=fixture.repositories['alpha'].id,
+                    contributor_alias_id=contributor_alias_objects[1].id,
+                    earliest_commit=get_date("2018-12-03"),
+                    latest_commit=datetime.utcnow() - timedelta(days=10),
+                    commit_count=100,
+                    contributor_id=contributor_objects[1].id,
+                    robot=False
+                )
+            )
+            api_helper.add_repository_contributor_alias(
+                dict(
+                    repository_id=fixture.repositories['alpha'].id,
+                    contributor_alias_id=contributor_alias_objects[2].id,
+                    earliest_commit=get_date("2018-12-03"),
+                    latest_commit=datetime.utcnow() - timedelta(days=10),
+                    commit_count=300,
+                    contributor_id=contributor_objects[2].id,
+                    robot=False
+                )
+            )
+
+            yield Fixture(
+                parent=fixture
+            )
+
+        def it_returns_all_contributors_with_their_aliases(self, setup):
+            fixture = setup
+            client = Client(schema)
+
+            with patch('polaris.analytics.service.graphql.account.Account.check_access', return_value=True):
+                response = client.execute(fixture.query, variable_values=dict(account_key=test_account_key))
+
+                assert 'data' in response
+                result = response['data']['account']
+                assert len(result['contributors']['edges']) == 3
+                contributors = result['contributors']['edges']
+                for c_node in contributors:
+                    contributor = c_node['node']
+                    assert contributor['id']
+                    assert contributor['key'] == contributor['contributorAliasesInfo'][0]['key']
+                    assert contributor['name'] == contributor['contributorAliasesInfo'][0]['name']
+                    assert contributor['latestCommit'] == contributor['contributorAliasesInfo'][0]['latestCommit']
+                    assert contributor['earliestCommit'] == contributor['contributorAliasesInfo'][0]['earliestCommit']
+                    assert contributor['commitCount'] == contributor['contributorAliasesInfo'][0]['commitCount']
+
+        class TestWithFilterWithoutInterface:
+            pass
 
     class TestWithFilterWithContributorAliasesInterface:
         pass

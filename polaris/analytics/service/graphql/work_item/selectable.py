c7adc461c43befc5d2bee6a49ff1584a1f2302b7
Pragya Goyal <pragya@64sqs.com>
2021-03-16 19:29:46 +0530

    Removed includeEpics check from WorkItemProgress interface resolver

--- polaris/analytics/service/graphql/work_item/selectable.py
+++ polaris/analytics/service/graphql/work_item/selectable.py
@@ -275,7 +275,7 @@ class WorkItemsWorkItemProgress(InterfaceResolver):
             ], else_=func.max(work_item_delivery_cycles.c.end_date))).label('end_date'),
             func.max(work_item_delivery_cycles.c.latest_commit).label('last_update')
         ]).select_from(
-            non_epic_work_items.join(
+            non_epic_work_items.outerjoin(
                 work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == non_epic_work_items.c.id
             )
         ).group_by(
@@ -306,69 +306,66 @@ class WorkItemsWorkItemProgress(InterfaceResolver):
             non_epic_work_items_span.c.last_update
         )
 
-        if kwargs.get('include_epics'):
-            epics = select([
-                work_item_nodes.c.id
-            ]).select_from(
-                work_item_nodes.join(
-                    work_items, work_item_nodes.c.id == work_items.c.id
-                )
-            ).where(
-                work_items.c.is_epic == True
-            ).cte()
-
-            epics_span = select([
-                epics.c.id,
-                (case([
-                    (func.sum(
-                        case([
-                            (work_item_delivery_cycles.c.end_date == None, 1)
-                        ], else_=0)) > 0, False)
-                ], else_=True)).label('closed'),
-                func.min(work_item_delivery_cycles.c.start_date).label('start_date'),
-                (case([
-                    (func.sum(case([
-                        (work_item_delivery_cycles.c.end_date == None, 1)
-                    ], else_=0)) > 0, None)
-                ], else_=func.max(work_item_delivery_cycles.c.end_date))).label('end_date'),
-                func.max(work_item_delivery_cycles.c.latest_commit).label('last_update')
-            ]).select_from(
-                epics.join(
-                    work_items, work_items.c.parent_id == epics.c.id
-                ).join(
-                    work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == work_items.c.id
-                )
-            ).group_by(
-                epics.c.id
-            ).alias()
-
-            epics_progress = select([
-                epics.c.id,
-                epics_span.c.closed,
-                epics_span.c.start_date,
-                epics_span.c.end_date,
-                epics_span.c.last_update,
-                (case([
-                    (epics_span.c.end_date == None,
-                     func.extract('epoch', datetime.now() - epics_span.c.start_date) / (1.0 * 3600 * 24))
-                ], else_=func.extract('epoch', epics_span.c.end_date - epics_span.c.start_date) / (
-                        1.0 * 3600 * 24))).label('elapsed')
-
-            ]).select_from(
-                epics.outerjoin(
-                    epics_span, epics_span.c.id == epics.c.id
-                )
-            ).group_by(
-                epics.c.id,
-                epics_span.c.closed,
-                epics_span.c.start_date,
-                epics_span.c.end_date,
-                epics_span.c.last_update
+        epics = select([
+            work_item_nodes.c.id
+        ]).select_from(
+            work_item_nodes.join(
+                work_items, work_item_nodes.c.id == work_items.c.id
             )
+        ).where(
+            work_items.c.is_epic == True
+        ).cte()
 
-            return epics_progress.union(non_epic_work_items_progress)
-        else:
-            return non_epic_work_items_progress
+        epics_span = select([
+            epics.c.id,
+            (case([
+                (func.sum(
+                    case([
+                        (work_item_delivery_cycles.c.end_date == None, 1)
+                    ], else_=0)) > 0, False)
+            ], else_=True)).label('closed'),
+            func.min(work_item_delivery_cycles.c.start_date).label('start_date'),
+            (case([
+                (func.sum(case([
+                    (work_item_delivery_cycles.c.end_date == None, 1)
+                ], else_=0)) > 0, None)
+            ], else_=func.max(work_item_delivery_cycles.c.end_date))).label('end_date'),
+            func.max(work_item_delivery_cycles.c.latest_commit).label('last_update')
+        ]).select_from(
+            epics.outerjoin(
+                work_items, work_items.c.parent_id == epics.c.id
+            ).outerjoin(
+                work_item_delivery_cycles, work_item_delivery_cycles.c.work_item_id == work_items.c.id
+            )
+        ).group_by(
+            epics.c.id
+        ).alias()
+
+        epics_progress = select([
+            epics.c.id,
+            epics_span.c.closed,
+            epics_span.c.start_date,
+            epics_span.c.end_date,
+            epics_span.c.last_update,
+            (case([
+                (epics_span.c.end_date == None,
+                 func.extract('epoch', datetime.now() - epics_span.c.start_date) / (1.0 * 3600 * 24))
+            ], else_=func.extract('epoch', epics_span.c.end_date - epics_span.c.start_date) / (
+                    1.0 * 3600 * 24))).label('elapsed')
+
+        ]).select_from(
+            epics.outerjoin(
+                epics_span, epics_span.c.id == epics.c.id
+            )
+        ).group_by(
+            epics.c.id,
+            epics_span.c.closed,
+            epics_span.c.start_date,
+            epics_span.c.end_date,
+            epics_span.c.last_update
+        )
+
+        return epics_progress.union(non_epic_work_items_progress)
 
 
 class WorkItemDeliveryCycleCycleMetrics(InterfaceResolver):

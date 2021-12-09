330664e7ed50027f1032a7b4a6a0e45a010c5c6b
Krishna Kumar <kkumar@exathink.com>
2021-12-09 15:41:19 -0600

    Remove the admin authorization check for this mutation.
    [PO-881]

--- polaris/analytics/service/graphql/work_item/mutations.py
+++ polaris/analytics/service/graphql/work_item/mutations.py
@@ -33,46 +33,41 @@ class ResolveCommitsForWorkItems(graphene.Mutation):
     success = graphene.Boolean()
 
     def mutate(self, info, resolve_commits_for_work_items_input):
-        # This is really an 'or' clause. Since it is hard to test graphql mutations
-        # without flask present, we are simply relying on the fact that current_user is
-        # null when we run in the test environment. Theoretically unsafe, but should be ok in
-        # any reasonable production scenario. 
-        if not current_user or 'admin' in current_user.roles_names:
-            with db.orm_session() as session:
-                work_item_summaries = []
-                for work_item_key in resolve_commits_for_work_items_input.work_item_keys:
-                    work_item = WorkItem.find_by_work_item_key(session, work_item_key)
-                    work_item_summaries.append(
-                        dict(
-                            key=work_item.key.hex,
-                            name=work_item.name,
-                            work_item_type=work_item.work_item_type,
-                            display_id=work_item.display_id,
-                            url=work_item.url,
-                            is_bug=work_item.is_bug,
-                            tags=work_item.tags,
-                            state=work_item.state,
-                            created_at=work_item.created_at,
-                            updated_at=work_item.updated_at,
-                            deleted_at=work_item.deleted_at,
-                            description=work_item.description,
-                            source_id=work_item.source_id,
-                            is_epic=work_item.is_epic,
-                            parent_key=work_item.parent.key if work_item.parent else None,
-                            parent_source_display_id=work_item.parent.display_id if work_item.parent else None,
-                            commit_identifiers=work_item.commit_identifiers,
-                            is_moved_from_current_source=work_item.is_moved_from_current_source
-                        )
+        with db.orm_session() as session:
+            work_item_summaries = []
+            for work_item_key in resolve_commits_for_work_items_input.work_item_keys:
+                work_item = WorkItem.find_by_work_item_key(session, work_item_key)
+                work_item_summaries.append(
+                    dict(
+                        key=work_item.key.hex,
+                        name=work_item.name,
+                        work_item_type=work_item.work_item_type,
+                        display_id=work_item.display_id,
+                        url=work_item.url,
+                        is_bug=work_item.is_bug,
+                        tags=work_item.tags,
+                        state=work_item.state,
+                        created_at=work_item.created_at,
+                        updated_at=work_item.updated_at,
+                        deleted_at=work_item.deleted_at,
+                        description=work_item.description,
+                        source_id=work_item.source_id,
+                        is_epic=work_item.is_epic,
+                        parent_key=work_item.parent.key if work_item.parent else None,
+                        parent_source_display_id=work_item.parent.display_id if work_item.parent else None,
+                        commit_identifiers=work_item.commit_identifiers,
+                        is_moved_from_current_source=work_item.is_moved_from_current_source
                     )
+                )
 
-                publish.resolve_commits_for_work_items(
-                    str(resolve_commits_for_work_items_input.organization_key),
-                    str(resolve_commits_for_work_items_input.work_items_source_key),
-                    work_item_summaries
-                )
-                return ResolveCommitsForWorkItems(
-                    success=True
-                )
+            publish.resolve_commits_for_work_items(
+                str(resolve_commits_for_work_items_input.organization_key),
+                str(resolve_commits_for_work_items_input.work_items_source_key),
+                work_item_summaries
+            )
+            return ResolveCommitsForWorkItems(
+                success=True
+            )
 
 
 class WorkItemsMutationsMixin:

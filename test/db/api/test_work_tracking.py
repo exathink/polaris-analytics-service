eafa23287178f795fb0dbf9514e3918329491bd0
priya <pmukundan0@gmail.com>
2023-10-27 12:21:21 -0400

    Initial changes for adding flagged to work_items

--- test/db/api/test_work_tracking.py
+++ test/db/api/test_work_tracking.py
@@ -119,6 +119,7 @@ class TestImportWorkItems(WorkItemsTest):
             assert row.releases == new_work_item['releases']
             assert row.story_points == new_work_item['story_points']
             assert row.sprints == new_work_item['sprints']
+            assert row.flagged == new_work_item['flagged']
 
     def it_is_idempotent(self, work_items_setup):
         organization_key, work_items_source_key = work_items_setup
